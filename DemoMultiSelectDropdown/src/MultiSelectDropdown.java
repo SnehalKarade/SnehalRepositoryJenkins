4cb62731bbf55e67e3f61a81cb5603a10f8523df
BHAGYASHRI <BHAGYASHRI@192.168.0.17>
2019-01-31 17:58:51 +0530

    Aanam

--- DemoMultiSelectDropdown/src/MultiSelectDropdown.java
+++ DemoMultiSelectDropdown/src/MultiSelectDropdown.java
@@ -36,7 +36,7 @@ public static void main(String[] args) {
 		System.out.println("Selected option "+i+" : " +selectedOptions.get(i).getText());
 	}
 	
-	System.out.println("Selected option");
+	System.out.println("Selected option =============================");
 	
 	objMultiselect.deselectAll();
 	
